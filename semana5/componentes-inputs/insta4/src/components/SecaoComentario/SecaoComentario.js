80411bd8e4978147dce443ecd757f40b5f422a1c
Érico Marshall <ericomarshall@MacBook-Pro-de-Erico.local>
2021-09-09 13:19:15 -0300

    atualização dos comentários no console

--- semana5/componentes-inputs/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ semana5/componentes-inputs/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -14,18 +14,19 @@ const InputComentario = styled.input`
 
 export class SecaoComentario extends Component {
 	state = {
-
+		valorComentario: ''
 	}
 
-	onChangeComentario() {
-
+	onChangeComentario = (event) => {
+		console.log(event.target.value);
+		this.setState({valorComentario: event.target.value})
 	}
 
 	render() {
 		return <CommentContainer>
 			<InputComentario
 				placeholder={'Comentário'}
-				value={''}
+				value={this.state.valorComentario}
 				onChange={this.onChangeComentario}
 			/>
 			<button onClick={this.props.aoEnviar}>Enviar</button>
