611195c55581f5b20462242d2465c50bdb64e7ee
Érico Marshall <ericogmarshall1@gmail.com>
2021-11-30 12:16:58 -0300

    endpoints de login, signup e buscar todos os usuários criados

---
+++ semana17/projeto/src/endpoints/getAllUsers.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import { connection } from "../data/connection";
+import { user } from "../types";
+
+export async function getAllUsers(
+   req: Request,
+   res: Response
+): Promise<void> {
+   try {
+      const token = req.headers.authorization as string
+
+      const page: number = Number(req.query.page) || 1
+
+      if (!token) {
+         res.statusCode = 401
+         throw new Error("Token inválido")
+      }
+
+      const offset = 10 * (page - 1)
+
+      const result = await connection.raw(`
+         SELECT user.id, user.name, user.email
+         FROM labecommerce_users AS users
+         ORDER BY name
+         LIMIT 10
+         OFFSET ${offset}
+      `)
+
+      const feed = result[0].map(toFeedItem)
+
+      res.status(200).send(feed)
+
+   } catch (error) {
+console.log(error);
+
+      if (res.statusCode === 200)
+         res.status(500).send("Um erro inesperado ocorreu =/")
+
+      else
+         res.send(error.message)
+   }
+}
+
+const toFeedItem = (input: any): user => input && {
+   id: input.id,
+   name: input.name,
+   email: input.email,
+   password: input.password
+}
--- semana17/projeto/src/index.ts
+++ semana17/projeto/src/index.ts
@@ -1,10 +1,17 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
 import { AddressInfo } from 'net'
+import { getAllUsers } from "./endpoints/getAllUsers"
+import { signup } from "./endpoints/signup"
+import { login } from "./endpoints/login"
 
 const app: Express = express();
 app.use(express.json());
 app.use(cors());
+
+app.post("/signup", signup)
+app.post("/login", login)
+app.get("/users", getAllUsers)
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
