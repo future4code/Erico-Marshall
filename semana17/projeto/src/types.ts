0ad29efade9fc673dfee5d12e10558306ae655de
Érico Marshall <ericogmarshall1@gmail.com>
2021-11-30 12:02:28 -0300

    ambiente e tabelas criadas

---
+++ semana17/projeto/src/types.ts
@@ -0,0 +1,14 @@
+
+export type user = {
+    id: string
+    name: string
+    email: string
+    password: string
+}
+
+export type product = {
+    id: string
+    name: string
+    price: number
+    image_url: string
+}
