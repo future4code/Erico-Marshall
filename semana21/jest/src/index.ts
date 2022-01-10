fa9261a2979be78ba70fba357445911e16a240e0
Érico Marshall <ericogmarshall1@gmail.com>
2022-01-10 13:10:01 -0300

    desafios feitos até o exercício 5

--- semana21/jest/src/index.ts
+++ semana21/jest/src/index.ts
@@ -1,6 +1,6 @@
 import knex from 'knex'
 import { config } from 'dotenv'
-import { User } from './model/userModel';
+import { User, UserChallenge, Result, LOCATION, NACIONALITY, Casino,  } from './model/interfaces';
 
 config()
 
@@ -14,14 +14,55 @@ export const performPurchase = (user: User, value: number): User | undefined => 
     return undefined
 }
 
-export const connection = knex({
-   client: "mysql",
-   connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_SCHEMA,
-      port: 3306,
-      multipleStatements: true
-   },
-});
+export function verifyAge(casino: Casino, users: UserChallenge[]): Result {
+    const allowed: UserChallenge[] = [];
+    const unallowed: UserChallenge[] = [];
+  
+    for (const user of users) {
+      if (casino.location === LOCATION.EUA) {
+        if (user.age >= 21) {
+          allowed.push(user);
+        } else {
+          unallowed.push(user);
+        }
+      } else if (casino.location === LOCATION.BRAZIL) {
+        if (user.age >= 18) {
+          allowed.push(user);
+        } else {
+          unallowed.push(user);
+        }
+        break;
+      }
+    }
+  
+    return {
+      brazilians: {
+        allowed: allowed
+          .filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
+          .map((u) => u.name),
+        unallowed: unallowed
+          .filter((user) => user.nacionality === NACIONALITY.BRAZILIAN)
+          .map((u) => u.name),
+      },
+      americans: {
+        allowed: allowed
+          .filter((user) => user.nacionality === NACIONALITY.AMERICAN)
+          .map((u) => u.name),
+        unallowed: unallowed
+          .filter((user) => user.nacionality === NACIONALITY.AMERICAN)
+          .map((u) => u.name),
+      },
+    };
+  }
+
+  export const connection = knex({
+    client: "mysql",
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA,
+        port: 3306,
+        multipleStatements: true
+    },
+  });
