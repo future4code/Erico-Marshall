662ade44fe4d04e67f35f63083487a41b6b747d1
Érico Marshall <ericogmarshall1@gmail.com>
2022-02-04 11:14:17 -0300

    Semana 22 - Maior Prefixo Comum

---
+++ semana22/maiorPrefixoComum/index.js
@@ -0,0 +1,20 @@
+function longestCommon(strs) {
+    if(strs.length === 0) return ""
+    
+    let commonPrefix = ""
+    for(let i = 0; i < strs[0].length; i++) {
+        let currentChar = strs[0][i]
+        
+        for(let j = 0; j < strs.length; j++) {
+            if(strs[j][i] !== currentChar) {
+                return commonPrefix
+            }
+        }
+        
+        if(currentChar) {
+            commonPrefix += currentChar
+        }
+    }
+    
+    return commonPrefix
+};
